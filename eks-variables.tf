5aad6e0cd905f04b455a64bd711bca9ec00020b5
maav-sk <shubhankarkhanda@gmail.com>
2025-12-28 21:54:30 +0530

    fix: Lower k8s version

--- eks-variables.tf
+++ eks-variables.tf
@@ -2,7 +2,7 @@
 variable "cluster_kubernetes_version" {
   description = "Kubernetes <major>.<minor> version to use for the cluster"
   type        = string
-  default     = "1.33"
+  default     = "1.30"
 }
 
 variable "cluster_access_entries" {
