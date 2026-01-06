27a235de99817805e4a0adbb40defdbfc25ec2ac
maav-sk <shubhankarkhanda@gmail.com>
2026-01-06 19:58:47 +0530

    remove region from backend

--- eks-infra/backend.tf
+++ eks-infra/backend.tf
@@ -3,7 +3,7 @@
 #   terraform init -backend-config="bucket=<bucket-name>" -backend-config="key=<tfvars-name>/terraform.tfstate"
 terraform {
   backend "s3" {
-    region       = var.region
+    # region is picked from AWS_REGION env var set by aws-actions/configure-aws-credentials
     encrypt      = true
     use_lockfile = true
   }
