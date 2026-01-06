3df9f384721e98b74b8ccb37890abe045dd6f1af
Dilip <dilip.samanta@gmail.com>
2026-01-06 13:31:24 +0530

    feat change strategy to call

--- eks-infra/variables.tf
+++ eks-infra/variables.tf
@@ -1,3 +1,31 @@
+################ORG INFO##########################
+variable "bu_id" {
+  description = "Business Unit"
+  type        = string
+  default     = null
+}
+
+variable "app_id" {
+  description = "application Unit"
+  type        = string
+  default     = null
+}
+
+variable "env" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+
+  validation {
+    condition     = can(regex("^(dev|staging|prod|test)$", var.env))
+    error_message = "Environment must be dev, staging, or prod"
+  }
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
 #######################VPC INFO#########################
 
 variable "vpc_cidr" {
